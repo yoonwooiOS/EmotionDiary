43c3c1c3d56423cfc491d5700bcc95bf51ad35dd
yoonwooiOS <yoonwoo4429@kakao.com>
2024-05-20 23:57:18 +0900

    UIButton, UILabel 디자인,  sender.tag로 버튼 9개 1개 버튼으로 동작

--- EmotionDiary/SubViewController.swift
+++ EmotionDiary/SubViewController.swift
@@ -4,6 +4,8 @@
 //
 //  Created by 김윤우 on 5/18/24.
 // 버튼 + 레이블, 버튼만, 컬렉션뷰
+// tag를 이용해서
+
 
 import UIKit
 
@@ -11,106 +13,80 @@ class SubViewController: UIViewController {
 
     @IBOutlet var backgroundImageView: UIImageView!
     
-   
+    @IBOutlet var emotionLabelList: [UILabel]!
     @IBOutlet var leftBarButtonItem: UIBarButtonItem!
     
     @IBOutlet var happyButton: UIButton!
     @IBOutlet var happyButtonLabel: UILabel!
-    var happyButtonCount = 0
+    
     
     @IBOutlet var loveButton: UIButton!
     @IBOutlet var loveButtonLabel:UILabel!
-    var loveButtonCount = 0
+    
     
     @IBOutlet var likeButton: UIButton!
     @IBOutlet var likeButtonLabel: UILabel!
-    var likeButtonCount = 0
+   
     
     @IBOutlet var embarrassmentButton: UIButton!
     @IBOutlet var embarrassmentButtonLabel: UILabel!
-    var embarrassmentButtonCount = 0
     
     @IBOutlet var upSetButton: UIButton!
     @IBOutlet var upSetButtonLabel: UILabel!
-    var upSetButtonCount = 0
-    
-    
+
     @IBOutlet var depressedButton: UIButton!
     @IBOutlet var depressedButtonLabel: UILabel!
-    var depressedButtonCount = 0
-    
+   
     @IBOutlet var boredButton: UIButton!
     @IBOutlet var boredButtonLabel: UILabel!
-    var boredButtonCount = 0
-    
+ 
     @IBOutlet var tiredButton: UIButton!
     @IBOutlet var tiredButtonLabel: UILabel!
-    var tiredButtonCount = 0
-    
+  
     @IBOutlet var sadButton: UIButton!
     @IBOutlet var sadButtonLabel: UILabel!
-    var sadButtonCount = 0
-    
     
     @IBOutlet var resetButton: UIButton!
     
-    
-    //Button Style이 Plain일 경우 contentmode가 적용되지 않음
+    var count = [0, 0, 0, 0, 0 ,0, 0, 0 ,0  ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // UI 구성
+        //Button Style이 Plain일 경우 contentmode가 적용되지 않음
         backgroundImageView.image = UIImage(named: "background")
         backgroundImageView.contentMode = .scaleToFill
         
         leftBarButtonItem.image = UIImage(systemName: "menucard")
         leftBarButtonItem.tintColor = .black
         
-        happyButton.setImage(UIImage(named: "slime1"), for: .normal)
-        happyButton.contentMode = .scaleToFill
-        happyButtonLabel.text = "행복해 \(happyButtonCount)"
-        happyButtonLabel.textAlignment = .center
+//        happyButton.setImage(UIImage(named: "slime1"), for: .normal)
+//        happyButton.contentMode = .scaleToFill
+//        happyButtonLabel.text = "행복해 \(happyButtonCount)"
+//        happyButtonLabel.textAlignment = .center
+        // Label
+        labelUiDesign(happyButtonLabel, "행복해", 0)
+        labelUiDesign(loveButtonLabel, "사랑해", 1)
+        labelUiDesign(likeButtonLabel, "좋아해", 2)
+        labelUiDesign(embarrassmentButtonLabel,  "당황해", 3)
+        labelUiDesign(upSetButtonLabel, "속상해", 4)
+        labelUiDesign(depressedButtonLabel, "우울해", 5)
+        labelUiDesign(boredButtonLabel, "지루해", 6)
+        labelUiDesign(tiredButtonLabel, "피곤해", 7)
+        labelUiDesign(sadButtonLabel, "슬퍼", 8)
         
-        loveButton.setImage(UIImage(named: "slime2"), for: .normal)
-        loveButton.contentMode = .scaleToFill
-        loveButtonLabel.text = "사랑해 \(loveButtonCount)"
-        loveButtonLabel.textAlignment = .center
+        //Button
+        buttonUiDesign(happyButton, "slime1")
+        buttonUiDesign(loveButton, "slime2")
+        buttonUiDesign(likeButton, "slime3")
+        buttonUiDesign(embarrassmentButton, "slime4")
+        buttonUiDesign(upSetButton, "slime5")
+        buttonUiDesign(depressedButton, "slime6")
+        buttonUiDesign(boredButton, "slime7")
+        buttonUiDesign(tiredButton, "slime8")
+        buttonUiDesign(sadButton, "slime9")
         
-        likeButton.setImage(UIImage(named: "slime3"), for: .normal)
-        likeButton.contentMode = .scaleToFill
-        likeButtonLabel.text = "좋아해 \(likeButtonCount)"
-        likeButtonLabel.textAlignment = .center
-        
-        embarrassmentButton.setImage(UIImage(named: "slime4"), for: .normal)
-        embarrassmentButton.contentMode = .scaleToFill
-        embarrassmentButtonLabel.text = "당황해 \(embarrassmentButtonCount)"
-        embarrassmentButtonLabel.textAlignment = .center
-        
-        upSetButton.setImage(UIImage(named: "slime5"), for: .normal)
-        upSetButton.contentMode = .scaleToFill
-        upSetButtonLabel.text = "속상해 \(upSetButtonCount)"
-        upSetButtonLabel.textAlignment = .center
-        
-        depressedButton.setImage(UIImage(named: "slime6"), for: .normal)
-        depressedButton.contentMode = .scaleToFill
-        depressedButtonLabel.text = "우울해 \(depressedButtonCount)"
-        depressedButtonLabel.textAlignment = .center
-        
-        boredButton.setImage(UIImage(named: "slime7"), for: .normal)
-        boredButton.contentMode = .scaleToFill
-        boredButtonLabel.text = "지루해 \(boredButtonCount)"
-        boredButtonLabel.textAlignment = .center
-        
-        tiredButton.setImage(UIImage(named: "slime8"), for: .normal)
-        tiredButton.contentMode = .scaleToFill
-        tiredButtonLabel.text = "피곤해 \(tiredButtonCount)"
-        tiredButtonLabel.textAlignment = .center
-        
-        sadButton.setImage(UIImage(named: "slime9"), for: .normal)
-        sadButton.contentMode = .scaleToFill
-        sadButtonLabel.text = "슬퍼 \(sadButtonCount)"
-        sadButtonLabel.textAlignment = .center
         
         resetButton.setTitle("감정 다시 정하기", for: .normal)
         resetButton.backgroundColor = .white
@@ -118,89 +94,80 @@ class SubViewController: UIViewController {
         resetButton.layer.cornerRadius = 10
     }
     
-    @IBAction func happyButtonTapped(_ sender: UIButton) {
-//        happyButtonCount += Int.random(in: 1...10)
-        happyButtonCount += 1
-        happyButtonLabel.text = "행복해 \(happyButtonCount)"
-    }
     
-    @IBAction func loveButtonTapped(_ sender: UIButton) {
-        loveButtonCount += 1
-        loveButtonLabel.text = "사랑해 \(loveButtonCount)"
-    }
     
-    @IBAction func likeButtonTapped(_ sender: UIButton) {
-        likeButtonCount += 1
-        likeButtonLabel.text = "좋아해 \(likeButtonCount)"
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        
+        switch sender.tag {
+        case 0:
+            count[sender.tag] += 1
+            happyButtonLabel.text = "행복해 \(count[sender.tag])"
+        case 1:
+            count[sender.tag] += 1
+            loveButtonLabel.text = "사랑해 \(count[sender.tag])"
+        case 2:
+            count[sender.tag] += 1
+            likeButtonLabel.text =
+            "좋아해 \(count[sender.tag])"
+        case 3:
+            count[sender.tag] += 1
+            embarrassmentButtonLabel.text =
+            "당황해 \(count[sender.tag])"
+        case 4:
+            count[sender.tag] += 1
+            upSetButtonLabel.text =
+            "속상해 \(count[sender.tag])"
+        case 5:
+            count[sender.tag] += 1
+            depressedButtonLabel.text =
+            "우울해 \(count[sender.tag])"
+        case 6:
+            count[sender.tag] += 1
+            boredButtonLabel.text =
+            "지루해 \(count[sender.tag])"
+        case 7:
+            count[sender.tag] += 1
+            tiredButtonLabel.text =
+            "피곤해 \(count[sender.tag])"
+        case 8:
+            count[sender.tag] += 1
+            sadButtonLabel.text =
+            "슬퍼 \(count[sender.tag])"
+        
+        default:
+           return
+        }
+        
     }
-    
-    @IBAction func embarrassmentButtonTapped(_ sender: UIButton) {
-        embarrassmentButtonCount += 1
-        embarrassmentButtonLabel.text = "당황해 \(embarrassmentButtonCount)"
-    }
-    
-    @IBAction func upSetButtonTapped(_ sender: UIButton) {
-        upSetButtonCount += 1
-        upSetButtonLabel.text = "속상해 \(upSetButtonCount)"
-    }
-    
-    @IBAction func depressedButtonTapped(_ sender: UIButton) {
-        depressedButtonCount += 1
-        depressedButtonLabel.text = "우울해 \(depressedButtonCount)"
-    }
-    
-    @IBAction func boredButtonTapped(_ sender: UIButton) {
-        boredButtonCount += 1
-        boredButtonLabel.text = "지루해 \(boredButtonCount)"
-
-    }
-    
-    @IBAction func tiredButtonTapped(_ sender: UIButton) {
-        tiredButtonCount += 1
-        tiredButtonLabel.text = "피곤해 \(tiredButtonCount)"
-
-    }
-    
-    @IBAction func sadButtonTapped(_ sender: UIButton) {
-        sadButtonCount += 1
-        sadButtonLabel.text = "슬퍼 \(sadButtonCount)"
-
-    }
-    
     
     @IBAction func resetButtonTapped(_ sender: UIButton) {
         
         reset()
 
     }
-    func reset() {
-        happyButtonCount = 0
-        happyButtonLabel.text = "행복해 \(happyButtonCount)"
-
-        loveButtonCount = 0
-        loveButtonLabel.text = "사랑해 \(loveButtonCount)"
-
-        likeButtonCount = 0
-        likeButtonLabel.text = "좋아해 \(likeButtonCount)"
-
-        embarrassmentButtonCount = 0
-        embarrassmentButtonLabel.text = "당황해 \(embarrassmentButtonCount)"
-
+    // label 디자인
+    func labelUiDesign(_ labelName: UILabel, _ emtion: String, _ number:Int) {
+        labelName.text = "\(emtion) \(count[number])"
+        labelName.textAlignment = .center
+    }
+    // button 디자인
+    func buttonUiDesign(_ button:UIButton, _ imageName:String) {
         
-        upSetButtonCount = 0
-        upSetButtonLabel.text = "속상해 \(upSetButtonCount)"
-
-        depressedButtonCount = 0
-        depressedButtonLabel.text = "우울해 \(depressedButtonCount)"
-
-        boredButtonCount = 0
-        boredButtonLabel.text = "지루해 \(boredButtonCount)"
-
-        tiredButtonCount = 0
-        tiredButtonLabel.text = "피곤해 \(tiredButtonCount)"
-
-        sadButtonCount = 0
-        sadButtonLabel.text = "슬퍼 \(sadButtonCount)"
+        button.setImage(UIImage(named: "\(imageName)"), for: .normal)
+        button.contentMode = .scaleToFill
+    }
+    
+    func reset() {
+        count = [0, 0, 0, 0, 0 ,0, 0, 0 ,0  ]
+        labelUiDesign(happyButtonLabel, "행복해", 0)
+        labelUiDesign(loveButtonLabel, "사랑해", 1)
+        labelUiDesign(likeButtonLabel, "좋아해", 2)
+        labelUiDesign(embarrassmentButtonLabel,  "당황해", 3)
+        labelUiDesign(upSetButtonLabel, "속상해", 4)
+        labelUiDesign(depressedButtonLabel, "우울해", 5)
+        labelUiDesign(boredButtonLabel, "지루해", 6)
+        labelUiDesign(tiredButtonLabel, "피곤해", 7)
+        labelUiDesign(sadButtonLabel, "슬퍼", 8)
     }
     
     
